7780c79b7541f165f98d6b0e3b7573d942b9c4d4
freakingrocky <rakshan793@gmail.com>
2024-06-20 16:15:09 +0800

    Initialize the Controllers

---
+++ Controllers/src/test/java/com/freakingrocky/trasy_controller/TrasyControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freakingrocky.trasy_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrasyControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
